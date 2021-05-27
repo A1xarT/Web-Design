86653035c1c6e536bd37c6c154fa093e70a738e2
Illya <A1xarT@ukr.net>
2021-05-27 19:34:22 +0300

    'mmm'

---
+++ api/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: 'localhost',
+    PORT: 5432,
+    DB: 'PhoneBook',
+    USER: 'postgres',
+    PASSWORD: '80601653'
+}
---
+++ api/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const pg = require('pg')
+const app = express(),
+port = 3080;
+
+// place holder for the data
+const users = [];
+
+app.get('/api/all-users', (req, res) => {
+    console.log('api/all-users called .')
+    const con = connectDatabase()
+    con.query('select * from _user', (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.send(result.rows)
+    })
+    con.end()
+})
+
+app.get('/api/users', (req, res) => {
+    console.log('api/users called!!!!!!!')
+    res.json(users);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
+
+function connectDatabase() {
+    const dbConfig = require('./db.config.js')
+    return new pg.Pool({
+        user: dbConfig.USER,
+        host: dbConfig.HOST,
+        database: dbConfig.DB,
+        password: dbConfig.PASSWORD,
+        port: dbConfig.PORT
+    })
+}
